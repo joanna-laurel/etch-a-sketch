bc0a1800ce0f8c8d414bd9c5acd4fa7b31c8edbc
joanna-laurel <191937222+joanna-laurel@users.noreply.github.com>
2025-04-18 17:07:27 -0400

    Add button and prompt for user to make a new grid
    If the user clicks the "start a new drawing button" they are prompted to
    enter a number from 10-100. Only entries within the specified range
    create a new grid.

--- script.js
+++ script.js
@@ -1,25 +1,56 @@
-// I don't think there's any way to make all the squares take up equal space
-// inside a container without splitting them into individual rows. So we need
-// a function that appends one row of x squares to containerDiv, then loop that
-// function x times while somehow adding starting a new row each time
 
-const grid = document.getElementById("containerDiv");
+const gridContainer = document.getElementById("gridContainer");
+const newGridButton = document.getElementById("newGridButton");
+newGridButton.addEventListener("click", askForNumber);
+// const numberPrompt = askForNumber();
+  
+function askForNumber() {
+  clearGrid();
+  // const numberPromptDiv = document.createElement("div");
+  // const numberPrompt = document.createElement("prompt");
+  // numberPromptDiv.classList.add("numberPromptDiv");
+  // numberPrompt.classList.add("numberPrompt");
+  // numberPrompt.type = "number";
+  // numberPromptDiv.textContent = "Enter a number from 10 to 100:";
+  // numberPromptDiv.appendChild(numberPrompt);
+  // gridContainer.appendChild(numberPromptDiv);
+  let userSelection = prompt("Please enter a number from 10 to 100 to create a new grid.");
+  validateSelection(userSelection);
+  function validateSelection(selection) {
+    if (selection === null) {
+      askForNumber();
+    } else {
+      selection = parseInt(selection);
+      if (!isNaN(selection) && selection >= 10 && selection <= 100) {
+        createGrid(selection);
+      } else {
+        alert("That's not a valid entry. Try again!");
+      }
+  }
+  } 
+}
 
 function addOneRow(num) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    grid.appendChild(row);
-    for (i = 0; i < num; i++) {
-      const square = document.createElement("div");
-      square.classList.add("square");
-      row.appendChild(square);
-    }
+  const row = document.createElement("div");
+  row.classList.add("row");
+  gridContainer.appendChild(row);
+  for (i = 0; i < num; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    row.appendChild(square);
+  }
 }
 
 function createGrid(num) {
-    for (x = 0; x < num; x++) {
-      addOneRow(num);
-    }
+  for (x = 0; x < num; x++) {
+    addOneRow(num);
+  }
 }
 
-createGrid(50);
+function clearGrid() {
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+} 
+
+createGrid(100);
