26ca24afc191dacf2154067df61f90e0fa9bf0a4
joanna-laurel <191937222+joanna-laurel@users.noreply.github.com>
2025-04-19 15:35:07 -0400

    Fix bugs preventing grid reset
    Everything functions as it should now. User can make as many successive
    sketches as they like.

--- script.js
+++ script.js
@@ -2,39 +2,40 @@
 const gridContainer = document.getElementById("gridContainer");
 const newGridButton = document.getElementById("newGridButton");
 
-let numberChoice = 100;
-createGrid(100);
 etchASketch();
 
 function etchASketch() {
-  newGridButton.addEventListener("click", askForNumber);
-  clearGrid();
-  createGrid(numberChoice);
-  // enableSketching(); //non-gradient sketch option
-  enableGradiatedSketching();
+  newGridButton.addEventListener("click", startSketching);
+
+  function startSketching() {
+    const number = askForNumber();
+    clearGrid();
+    createGrid(number);
+    enableGradiatedSketching();
+  }
 }
   
 function askForNumber() {
-  let userSelection = prompt("Please enter a number from 10 to 100 to create a new grid.", "100");
-  userSelection = parseInt(userSelection);
-  if (!isNaN(userSelection) && userSelection >= 10 && userSelection <= 100) {
-    numberChoice = userSelection;
+  let numberChoice = prompt("Please enter a number from 10 to 100 to create a new grid.", "100");
+  numberChoice = Number.parseInt(numberChoice);
+  if (!isNaN(numberChoice) && numberChoice >= 10 && numberChoice <= 100) {
+    return numberChoice;
   } else {
     alert("That's not a valid entry. Try again!");
   }
 }
 
-function addOneRow(num) {
-  const row = document.createElement("div");
-  row.classList.add("row");
-  gridContainer.appendChild(row);
-  for (i = 0; i < num; i++) {
-    const square = document.createElement("div");
-    // square.classList.add("square"); //coordinates with enableSketching function
-    square.classList.add("gradientSquare");
-    row.appendChild(square);
-  }
-}
+// function addOneRow(num) { //black-and-white gradient sketching function
+//   const row = document.createElement("div");
+//   row.classList.add("row");
+//   gridContainer.appendChild(row);
+//   for (i = 0; i < num; i++) {
+//     const square = document.createElement("div");
+//     // square.classList.add("square"); //coordinates with enableSketching function
+//     square.classList.add("gradientSquare");
+//     row.appendChild(square);
+//   }
+// }
 
 function addRandomColorsRow(num) {
   const row = document.createElement("div");
@@ -43,12 +44,10 @@ function addRandomColorsRow(num) {
   for (i = 0; i < num; i++) {
     const square = document.createElement("div");
     square.classList.add("gradientSquareRandomColor");
-    // square.classList.add("gradientSquare");
     let r = Math.floor(Math.random() * 255);
     let g = Math.floor(Math.random() * 255);
     let b = Math.floor(Math.random() * 255);
     const randomColor = "(" + r + ", " + g + ", " + b + ")";
-    // console.log(randomColor);
     square.style.backgroundColor = ("rgb" + randomColor);
     row.appendChild(square); 
   }
@@ -56,7 +55,6 @@ function addRandomColorsRow(num) {
 
 function createGrid(num) {
   for (x = 0; x < num; x++) { 
-    // addOneRow(num);
     addRandomColorsRow(num);
   }
 }
@@ -67,7 +65,7 @@ function clearGrid() {
   }
 } 
 
-// function enableSketching() { //this is the pre-gradient sketching function
+// function enableSketching() { //pre-gradient sketching function
 //   console.log("sketchy")
 //   document.querySelectorAll(".square").forEach(item => 
 //     item.addEventListener("mousemove", event => 
@@ -86,7 +84,6 @@ function enableGradiatedSketching() {
     const currentOpacity = computedStyle.getPropertyValue("opacity");
     if (currentOpacity < 1) {
       const newOpacity = parseFloat(currentOpacity) + .1;
-      // console.log(newOpacity);
       this.style.opacity = newOpacity;
     }
   }
